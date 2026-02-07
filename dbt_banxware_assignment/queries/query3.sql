4255895387db98bd8d3b68ff6742887b11b1511b
Raul Perez <121673533+rbravez@users.noreply.github.com>
2026-02-07 12:07:16 +0100

    update query3

--- dbt_banxware_assignment/queries/query3.sql
+++ dbt_banxware_assignment/queries/query3.sql
@@ -1,5 +1,6 @@
 SELECT 
-    order_month, ROUND(AVG(total_sales_amount), 2)
-FROM HOME_ASSIGNMENT.PUBLIC.TRANSFORMED_SALES_DATA
-GROUP BY order_month
-ORDER BY order_month ASC
+ order_month, ROUND(AVG(total_sales_amount), 2)
+ FROM HOME_ASSIGNMENT.PUBLIC.TRANSFORMED_SALES_DATA
+ WHERE order_year = 2023
+ GROUP BY order_month
+ ORDER BY order_month ASC
