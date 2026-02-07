132013f9c0b7ef0e2ca02ee4f350f9fa6c233f76
Raul Perez <121673533+rbravez@users.noreply.github.com>
2026-02-07 12:06:57 +0100

    update query2

--- dbt_banxware_assignment/queries/query2.sql
+++ dbt_banxware_assignment/queries/query2.sql
@@ -1,8 +1,17 @@
-SELECT 
-    c.name
-FROM HOME_ASSIGNMENT.PUBLIC.TRANSFORMED_SALES_DATA s
-JOIN HOME_ASSIGNMENT.PUBLIC.RAW_CUSTOMER_DATA c ON s.customer_id = c.id
-WHERE s.order_year = 2023
-GROUP BY c.id, c.name
-ORDER BY SUM(s.total_sales_amount) DESC
-LIMIT 5;
+    WITH top_customers AS (
+ SELECT 
+     customer_id,
+     SUM(total_sales_amount) AS total_spent
+ FROM HOME_ASSIGNMENT.PUBLIC.TRANSFORMED_SALES_DATA
+ WHERE order_year = 2023
+ GROUP BY customer_id
+ ORDER BY total_spent DESC
+ LIMIT 5
+ )
+
+ SELECT 
+     c.name,
+     t.total_spent
+ FROM top_customers t
+ JOIN HOME_ASSIGNMENT.PUBLIC.RAW_CUSTOMER_DATA c ON t.customer_id = c.id
+ ORDER BY t.total_spent DESC;
